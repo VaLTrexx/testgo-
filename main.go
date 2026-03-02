3a592ba050c9fc71b91fcc63201f2c9a43f99b5e
Abhinav Paste <valtrexcode01@gmail.com>
2026-03-02 17:02:05 +0530

    a man with no pants does not fear the pocket picker

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
-import(
+
+import (
 	"context"
-	"log"
-	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type Task struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var db *pgxpool.Pool
+
+func main() {
+	dsn := "postgres://postgres:abhinav@localhost:5432/test"
+
+	db, _ = pgxpool.New(context.Background(), dsn)
+
+	r := gin.Default()
+
+	r.POST("/tasks", createTask)
+	r.GET("/tasks", getTasks)
+	r.GET("/tasks/:id", getTask)
+	r.PUT("/tasks/:id", updateTask)
+
+	r.Run(":8080")
+
+}
