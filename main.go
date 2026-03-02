7a5f405c10fb7409c423d802e29afd4d3379dd41
Abhinav Paste <valtrexcode01@gmail.com>
2026-03-02 17:15:07 +0530

    every dead body on everest was once a motivated soul

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -28,5 +29,90 @@ func main() {
 	r.PUT("/tasks/:id", updateTask)
 
 	r.Run(":8080")
+}
 
+func createTask(c *gin.Context) {
+	var task Task
+
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := db.QueryRow(
+		context.Background(),
+		"INSERT INTO tasks (title, completed) VALUES ($1, $2) RETURNING id",
+		task.Title,
+		task.Completed,
+	).Scan(&task.ID)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+func getTasks(c *gin.Context) {
+	rows, err := db.Query(context.Background(), "SELECT id, title, completed FROM tasks")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var tasks []Task
+
+	for rows.Next() {
+		var t Task
+		rows.Scan(&t.ID, &t.Title, &t.Completed)
+		tasks = append(tasks, t)
+	}
+
+	c.JSON(http.StatusOK, tasks)
+}
+
+func getTask(c *gin.Context) {
+	id := c.Param("id")
+
+	var task Task
+	err := db.QueryRow(
+		context.Background(),
+		"SELECT id, title, completed FROM tasks WHERE id=$1",
+		id,
+	).Scan(&task.ID, &task.Title, &task.Completed)
+
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+func updateTask(c *gin.Context) {
+	id := c.Param("id")
+	var task Task
+
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err := db.Exec(
+		context.Background(),
+		"UPDATE tasks SET title=$1, completed=$2 WHERE id=$3",
+		task.Title,
+		task.Completed,
+		id,
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	task.ID = 0
+	c.JSON(http.StatusOK, gin.H{"message": "updated"})
 }
